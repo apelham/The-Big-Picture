4bea20ce203fb980921da53659cd49e1bea79ce7
apelham <alpelham32@gmail.com>
2019-12-13 16:50:29 -0500

    Uipart?

---
+++ Assets/Scripts/Lvl6C.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lvl6C : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        //Application.LoadLevel(2);
+        SceneManager.LoadScene(1);
+    }
+}
+
